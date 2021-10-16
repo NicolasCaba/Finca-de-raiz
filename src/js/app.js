fb483ae914e053b5f5207dc4b91db1966e195743
Nicolás Caballero <67204635+NicolasCaba@users.noreply.github.com>
2021-10-16 03:04:14 -0500

    Se agrego modo oscuro con css y js, archivos de configuracion de eslint y prettier

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,42 @@
-"use strict";
-document.addEventListener("DOMContentLoaded", function () {
-    eventListeners();
-});
+function navegacionResponsive() {
+  const NAVEGACION = document.querySelector('.navegacion');
+
+  NAVEGACION.classList.toggle('mostrar');
+}
+
+function darkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+
+function autoDarkMode() {
+  const COLOR_PREFERENCE_DARK_MODE = window.matchMedia(
+    '(prefers-color-scheme: dark)',
+  );
+
+  if (COLOR_PREFERENCE_DARK_MODE.matches) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+
+  COLOR_PREFERENCE_DARK_MODE.addEventListener('change', () => {
+    if (COLOR_PREFERENCE_DARK_MODE.matches) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  });
+}
 
 function eventListeners() {
-    const MOBILE_MENU = document.querySelector(".mobile-menu");
+  const MOBILE_MENU = document.querySelector('.mobile-menu');
+  const BOTON_DARK_MODE = document.querySelector('.dark-mode-boton');
 
-    MOBILE_MENU.addEventListener("click", navegacionResponsive);
+  MOBILE_MENU.addEventListener('click', navegacionResponsive);
+  BOTON_DARK_MODE.addEventListener('click', darkMode);
 }
 
-function navegacionResponsive() {
-    const NAVEGACION = document.querySelector(".navegacion");
-
-    NAVEGACION.classList.toggle("mostrar");
-}
+document.addEventListener('DOMContentLoaded', () => {
+  eventListeners();
+  autoDarkMode();
+});
